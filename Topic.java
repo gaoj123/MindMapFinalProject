fe8184f4b54a311a830b378fa195c7278d571a7a
Jenny Gao <jgao658@gmail.com>
2017-05-31 12:34:21 -0400

    added constructor and methods

--- Topic.java
+++ Topic.java
@@ -2,5 +2,14 @@ public class Topic{
     ArrayList<Topic> subtopics=new ArrayList<Topic>;
     String text;
     int fontSize;
-    Body body=new Body();
+    //create object after getting x value from text field
+    public Topic(){
+	text="";
+    }
+    public void modifyLabel(String x){
+	text=x;
+    }
+    public void addSubtopic(Topic children){
+	subtopics.add(children);
+    }
 }
