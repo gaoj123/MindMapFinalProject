1bebaf6d11ac33b8e6ef517fa9caf57c9ce81044
Jenny Gao <jgao658@gmail.com>
2017-06-03 18:46:09 -0400

    adding objects to instance var (array list of topics)

--- MindMap.java
+++ MindMap.java
@@ -1,5 +1,10 @@
 public class MindMap{
+    ArrayList<Topic> allObjects=new ArrayList<Topic>();
     public MindMap(){
+    }
+    //add topic objects to diagram
+    public void addToDiag(Topic toAdd){
+	allObjects.add(toAdd);
     }
     public void update(){
     }
